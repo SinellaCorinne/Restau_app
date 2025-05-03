2bfe139c7082e7adde9141bb431250351046c8fe
ShalomEphrath <shalombambelou@gmail.com>
2025-05-03 23:07:03 +0200

    auth started

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\CreateUserRequest;
+use App\Http\Requests\Api\LoginRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(CreateUserRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'user' => new UserResource($user)
+        ], 201);
+    }
+
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $data = $request->validated();
+
+        $user = User::where('email', $data['email'])->first();
+
+        if (empty($user) || ! Hash::check($data['password'], $user->password)) {
+            return response()->json(['message' => 'The provided credentials are incorrect.'], 401);
+        }
+
+        $token = $user->createToken('auth_token', ['*'], now()->addDay())->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function user(Request $request): UserResource
+    {
+        return new UserResource($request->user());
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
+}
